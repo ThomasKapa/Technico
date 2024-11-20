a00d3bb244a3aa0b64f5ec53e78eb23306d3f433
sfarantos <spyrosfarantos@gmail.com>
2024-11-20 22:43:02 +0200

    add PropertyRepository

--- technico/src/main/java/com/technicoCompany/technico/repository/PropertyRepository.java
+++ technico/src/main/java/com/technicoCompany/technico/repository/PropertyRepository.java
@@ -1,4 +1,19 @@
 package com.technicoCompany.technico.repository;
 
-public class PropertyRepository {
+import com.technicoCompany.technico.model.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+
+    // βγαζει property με βαση το αφμ
+    List<Property> findByOwnerVatNumber(String vatNumber);
+
+    // βγαζει property με βαση το address
+    Optional<Property> findByAddress(String address);
+
+    // βγαζει oλα τα property του owner με βαση το αφμ
+    List<Property> findByOwnerVatNumberOrderByAddress(String vatNumber);
 }

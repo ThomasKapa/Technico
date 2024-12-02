335e9f731d7a606e2e293bbf3d08e67b67201c07
xtkaravas <xtkaravas@ote.gr>
2024-12-02 13:04:14 +0200

    repairserviceTest

---
+++ technico/src/test/java/com/technicoCompany/technico/RepairServiceTest.java
@@ -0,0 +1,14 @@
+package com.technicoCompany.technico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RepairServiceTest {
+
+    @Test
+    public  void findRepairByIdTest (){
+
+    }
+
+}
